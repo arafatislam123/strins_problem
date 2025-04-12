fecd08507e427b27d49cdb7f3f77039d2eacd00d
Arafat420 <arafatpstu7@gmail.com>
2025-04-12 15:42:45 +0600

    first commit

---
+++ A_String_Task.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define ff first
+#define ss second
+#define nl '\n'
+#define pii pair<int, int>
+#define pll pair<long long int, long long int>
+#define ALL(v) (v).begin(), (v).end()
+#define rALL(v) (v).rbegin(), (v).rend()
+
+#define show(x) cout << #x << " : " << x << endl
+
+void solve(int tc)
+{
+    string s;
+    cin >> s;
+    for (char ch : s)
+    {
+        ch = tolower(ch);
+        if (ch == 'a' || ch == 'o' || ch == 'y' || ch == 'e' || ch == 'u' || ch == 'i')
+        {
+            continue;
+        }
+        cout << "." << ch;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tc = 1;
+    // cin >> tc;
+
+    for (int i = 1; i <= tc; i++)
+        solve(i);
+}
